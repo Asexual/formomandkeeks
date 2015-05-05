1aab9bd086c7da3269c1ad0dc28f7dbb6db47a00
Chow <dogevpn@gmail.com>
2015-05-05 02:38:15 -0500

    c file that he made
    Merry Christmas to his mother!

---
+++ formomandkeeks.c
@@ -0,0 +1,59 @@
+// for keeks and mom, merry christmas
+#include <stdio.h>
+
+void tree(void);
+void santa(void);
+
+int main(int argc, char * argv[])
+{
+    tree();
+	santa();
+
+	printf("Merry Christmas!");
+
+    return 0;
+}
+
+void tree()
+{
+printf("                 \ /");
+printf("               -->*<--");
+printf("                 /_\");
+printf("                /_\_\");
+printf("               /_/_/_\");
+printf("              /_\_\_\_\");
+printf("             /_/_/_/_/_\");
+printf("            /_\_\_\_\_\_\");
+printf("           /_/_/_/_/_/_/_\");
+printf("          /_\_\_\_\_\_\_\_\");
+printf("         /_/_/_/_/_/_/_/_/_\");
+printf("        /_\_\_\_\_\_\_\_\_\_\");
+printf("       /_/_/_/_/_/_/_/_/_/_/_\");
+printf("                [___]");
+}
+
+void santa()
+{
+printf("                 (___)");
+printf("                  / \");
+printf("                 /   \");
+printf("                /     \");
+printf("               /       \");
+printf("              /         \");
+printf("             /           \");
+printf("            /             \");
+printf("           /_______________\");
+printf("          [_________________]");
+printf("         / / (_o_)   (_o_) \ \");
+printf("        (_(______,   ,______)_)");
+printf("       ( \      .`---'.      / )");
+printf("       ( /`~""'`  ._.  `'""~`\ )");
+printf("        (                     )");
+printf("       (                       )");
+printf("      (                         )");
+printf("      (                         )");
+printf("       (                       )");
+printf("        (                     )");
+printf("         `(                 )'");
+printf("           `"~'---,.,---'~"`");
+}
